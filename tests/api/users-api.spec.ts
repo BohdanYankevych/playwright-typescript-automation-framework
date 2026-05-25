590de6ebd9ddeb2e85ed5375420244b770f3c68a
Bohdan_Yankevych <777janek777@gmail.com>
2026-05-25 16:42:07 +0200

    Add POST API test

--- tests/api/users-api.spec.ts
+++ tests/api/users-api.spec.ts
@@ -12,3 +12,21 @@ test('GET users list', async ({ request }) => {
   expect(responseBody[0]).toHaveProperty('email');
   expect(responseBody[0]).toHaveProperty('name');
 });
+test('POST create new post', async ({ request }) => {
+  const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
+    data: {
+      title: 'QA Automation',
+      body: 'Playwright API test',
+      userId: 1,
+    },
+  });
+
+  expect(response.status()).toBe(201);
+
+  const responseBody = await response.json();
+
+  expect(responseBody).toHaveProperty('id');
+  expect(responseBody.title).toBe('QA Automation');
+  expect(responseBody.body).toBe('Playwright API test');
+  expect(responseBody.userId).toBe(1);
+});
