bcc32cb1f348a106e797e558e9b0c4dca9a892ff
Bohdan_Yankevych <777janek777@gmail.com>
2026-06-13 20:51:06 +0200

    Update GitHub Actions to run API tests

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -1,9 +1,29 @@
-import { test, expect } from '../fixtures/test-fixtures';
+import { expect } from '@playwright/test';
+import * as allure from 'allure-js-commons';
+import { test } from '../fixtures/test-fixtures';
 
-test('@smoke successful login to SauceDemo', async ({ page, loginPage }) => {
-  await loginPage.openLoginPage();
+test('@smoke successful login to SauceDemo', async ({
+  page,
+  loginPage,
+}) => {
+  await allure.owner('Bohdan Yankevych');
+  await allure.severity('critical');
+  await allure.epic('SauceDemo');
+  await allure.feature('Authentication');
+  await allure.story('Successful Login');
 
-  await loginPage.login(process.env.USERNAME!, process.env.PASSWORD!);
+  await allure.step('Open login page', async () => {
+    await loginPage.openLoginPage();
+  });
 
-  await expect(page).toHaveURL(/inventory/);
+  await allure.step('Login with valid credentials', async () => {
+    await loginPage.login(
+      process.env.USERNAME!,
+      process.env.PASSWORD!,
+    );
+  });
+
+  await allure.step('Verify user is redirected to inventory page', async () => {
+    await expect(page).toHaveURL(/inventory/);
+  });
 });
